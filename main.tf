63bad9a899bf13f68c2e9308b0a857d30e179bfb
reachmohancareers88 <reachmohancareers@gmail.com>
2026-05-21 17:08:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,26 +2,22 @@ provider "azurerm" {
   features {}
 }
 
-
 resource "null_resource" "vm_manage" {
 
   depends_on = [null_resource.ip_manage]
 
   provisioner "local-exec" {
-    #when = "create" # This line is optional as it's the default
-    command = "az vm start --resource-group denmark-east-rg --name workstation"
+    command = "az vm start --resource-group denmark-east-rg --name Controller"
   }
-
 
   provisioner "local-exec" {
-    when = "destroy"
-    command = "az vm stop --resource-group denmark-east-rg --name workstation ; az vm deallocate --resource-group denmark-east-rg --name workstation"
+    when    = destroy
+    command = "az vm deallocate --resource-group denmark-east-rg --name Controller"
   }
-
 }
 
 resource "azurerm_public_ip" "workstation" {
-  name                = "workstation-public-ip"
+  name                = "controller-public-ip"
   location            = "Denmark East"
   resource_group_name = "denmark-east-rg"
   allocation_method   = "Static"
@@ -32,15 +28,13 @@ resource "null_resource" "ip_manage" {
   depends_on = [azurerm_public_ip.workstation]
 
   provisioner "local-exec" {
-    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name workstation409_z1 --name ipconfig1 --public-ip-address workstation-public-ip"
+    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name controller917_z1 --name ipconfig1 --public-ip-address controller-public-ip"
   }
-
 
   provisioner "local-exec" {
-    when = "destroy"
-    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name workstation409_z1 --name ipconfig1 --public-ip-address null"
+    when    = destroy
+    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name controller917_z1 --name ipconfig1 --public-ip-address null"
   }
-
 }
 
 output "ip" {
@@ -62,7 +56,7 @@ resource "azurerm_public_ip" "natgw" {
 }
 
 resource "azurerm_nat_gateway" "main" {
-  name                    = "workstation-natgw"
+  name                    = "controller-natgw"
   location                = "Denmark East"
   resource_group_name     = "denmark-east-rg"
   sku_name                = "Standard"
@@ -75,6 +69,11 @@ resource "azurerm_nat_gateway_public_ip_association" "main" {
 }
 
 resource "azurerm_subnet_nat_gateway_association" "main" {
+
+  depends_on = [
+    azurerm_nat_gateway_public_ip_association.main
+  ]
+
   subnet_id      = data.azurerm_subnet.default.id
   nat_gateway_id = azurerm_nat_gateway.main.id
 }
